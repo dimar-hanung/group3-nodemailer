e68dd23e281914d7bb136c3dc6f73d7ca57cdeb9
dimar hanung <42312636+dimar-hanung@users.noreply.github.com>
2020-08-11 20:25:53 +0700

    add handling error

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const { User } = require("../db/models");
+const { Error } = require("sequelize");
 const response = {
   status: false,
   data: [],
@@ -6,18 +7,16 @@ const response = {
 
 class UserController {
   static async getUser(req, res) {
-    const data = await User.findAll({}).then(function (data) {
-      return data;
-    });
-    response.status = data?"Success" : false
+    const data = await User.findAll({}).catch((err) => err);
+    response.status = data ? "Success" : false;
     response.data = data;
     res.json(response);
   }
 
   static async getUserById(req, res) {
-    const data = await User.findByPk(req.params.id);
-    response.status = data?"Success":false;
-    response.data = data?data:{}
+    const data = await User.findByPk(req.params.id).catch((err) => err);
+    response.status = data ? "Success" : false;
+    response.data = data ? data : {};
     res.json(response);
   }
 
@@ -26,28 +25,28 @@ class UserController {
     const data = await User.update(
       { username, email, password },
       { where: { id: req.params.id } }
-    );
-    response.status = data[0] ? "Success" : false
+    ).catch((err) => err);
+    response.status = data[0] ? "Success" : false;
     response.data = data[0] ? req.body : {};
     res.json(response);
   }
 
   static async deleteUser(req, res) {
-    console.log(req.params)
+    console.log(req.params);
     const data = await User.destroy({
       where: { id: req.params.id },
-    });
+    }).catch((err) => err);
     response.status = data ? "Success, data deleted" : false;
     response.data = data ? req.body : {};
-    res.json(response)
+    res.json(response);
   }
   static async saveUser(req, res) {
     const { body } = req;
-    const data = await User.build(body);
-    data.save()
+    const data = await User.build(body).catch((err) => err);
+    data.save();
     response.status = data ? "Success" : false;
-    response.data = data ? body : {}; 
-    res.json(response)
+    response.data = data ? body : {};
+    res.json(response);
   }
 }
 
